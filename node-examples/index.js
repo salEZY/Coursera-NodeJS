f5480d7174f9e88abb033db9335dc67d586f9088
salEZY <bobicaleks@yahoo.com>
2020-09-29 11:58:41 +0200

    first node examples

---
+++ node-examples/index.js
@@ -0,0 +1,22 @@
+const rect = {
+  perimeter: (x, y) => 2 * (x + y),
+  area: (x, y) => x * y,
+};
+
+const solveRect = (l, b) => {
+  console.log(`Solving rectangle with l = ${l} and b = ${b}`);
+
+  if (l <= 0 || b <= 0) {
+    console.log(
+      `Rectangle dimensions should be greater than zero: l = ${l} , b = ${b}`
+    );
+  } else {
+    console.log(`The area of the rectangle is ${rect.area(l, b)}`);
+    console.log(`The perimeter of the rectangle is ${rect.perimeter(l, b)}`);
+  }
+};
+
+solveRect(2, 4);
+solveRect(3, 5);
+solveRect(0, 5);
+solveRect(10, -3);
