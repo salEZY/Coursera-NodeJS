12fe6240ba5dc6fb2672d17bd65b91e56c15b388
salEZY <bobicaleks@yahoo.com>
2020-09-29 12:03:06 +0200

    first node module

--- node-examples/index.js
+++ node-examples/index.js
@@ -1,7 +1,4 @@
-const rect = {
-  perimeter: (x, y) => 2 * (x + y),
-  area: (x, y) => x * y,
-};
+const rect = require("./rectangle");
 
 const solveRect = (l, b) => {
   console.log(`Solving rectangle with l = ${l} and b = ${b}`);
