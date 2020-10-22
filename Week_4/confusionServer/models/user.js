a13fbf2cfd3d2a05ed1f22733967dd8216ffa0e4
AleksBobic <bobicaleks@yahoo.com>
2020-10-22 08:43:18 +0200

    Week 4 start

---
+++ Week_4/confusionServer/models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const userSchema = new Schema(
+  {
+    firstname: {
+      type: String,
+      default: "",
+    },
+    lastname: {
+      type: String,
+      default: "",
+    },
+    admin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("User", userSchema);
---
+++ Week_4/confusionServer/routes/users.js
@@ -0,0 +1,82 @@
+var express = require("express");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
+const Users = require("../../../Week_3/confusionServer/models/user");
+const authenticate = require("../authenticate");
+
+var router = express.Router();
+router.use(bodyParser.json());
+
+/* GET users listing. */
+router.get("/", authenticate.verifyUser, authenticate.verifyAdmin, function (
+  req,
+  res,
+  next
+) {
+  Users.find({})
+    .then(
+      (users) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(users);
+      },
+      (err) => next(err)
+    )
+    .catch((err) => next(err));
+});
+
+router.post("/signup", (req, res, next) => {
+  Users.register(
+    new Users({ username: req.body.username }),
+    req.body.password,
+    (err, user) => {
+      if (err) {
+        res.statusCode = 500;
+        res.setHeader("Content-Type", "application/json");
+        res.json({ err: err });
+      } else {
+        if (req.body.firstname) {
+          user.firstname = req.body.firstname;
+        }
+        if (req.body.lastname) {
+          user.lastname = req.body.lastname;
+        }
+        user.save((err, user) => {
+          if (err) {
+            res.statusCode = 500;
+            res.setHeader("Content-Type", "application/json");
+            res.json({ err: err });
+            return;
+          }
+          passport.authenticate("local")(req, res, () => {
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "application/json");
+            res.json({ success: true, status: "Registration successful" });
+          });
+        });
+      }
+    }
+  );
+});
+
+router.post("/login", passport.authenticate("local"), (req, res, next) => {
+  let token = authenticate.getToken({ _id: req.user._id });
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+  res.json({ success: true, token: token, status: "You are logged in!" });
+});
+
+router.get("/logout", (req, res, next) => {
+  if (req.session) {
+    req.session.destroy();
+    res.clearCookie("session-id");
+    res.redirect("/");
+  } else {
+    let err = new Error("You are not logged in");
+    err.status = 403;
+    next(err);
+  }
+});
+
+module.exports = router;
